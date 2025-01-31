1e527d083b1202410b062fbf808f1f86456c39a4
Jarvis Robert Coghlin <jarviscoghlin@gmail.com>
2025-01-30 22:00:12 -0500

    Hello world

--- Lox.swift
+++ Lox.swift
@@ -2,5 +2,22 @@
 
 print("Hello world")
 
+Lox.main()
+struct Lox {
+    static func main () {
 
+        print("Running Lox...")
+
+        let arguments = CommandLine.arguments
+        print(arguments)
+
+        for arg in arguments {
+            print("\nType, arg")
+            print(type(of: arg))
+            print(arg)
+        }
+
+        
+    }
+}
 
